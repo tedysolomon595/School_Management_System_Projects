7a1d5076179210a3eb41ae3775fedd649aae5af9
TEDY SOLOMON <tedys2662@gmail.com>
2022-05-25 16:11:00 +0300

    Seventh time edition

--- src/Components/Achievements.jsx
+++ src/Components/Achievements.jsx
@@ -1,32 +1,23 @@
 import React from 'react'
 import AchievementsText from './AchievementsText'
 export default function Achievements() {
-    const achievements=[
-        "../img/Students/img_8.jpg",
-        "../img/Students/img_9.jpg",
-        "../img/Students/img_10.jpg",
-        "../img/Students/img_11.jpg",
-        "../img/Students/img_12.jpg",
-        "../img/Students/img_13.jpg"
-    ]
-    const [count,setCount]=React.useState(0)
-    
+   
   return (
     <div className='Achievements'>
         <h2 className='title'>Achievements </h2>
         <AchievementsText/>
-        <div className='students-img-container'>
-            {achievements.map((img,index)=>(
-                count===index&&<div className='student-img'>
-                    <img src={img} alt='' />
-                </div>
-            ))}
-        </div>
-        
-        <div className='circle-button'>
-            {achievements.map((img,index)=>(
-                <span className={count===index?'circle active':'circle'} role='button' tabIndex={index} onClick={()=>setCount(index)}></span>
-            ))}
+        <div className='students-container'>
+            <div className='text-container'>
+               <h2 className='title'>Our Laboratory is organized by :</h2>
+               <p>Full of laboratory equipments and chemicals</p>
+               <p>The manuals are prepared properly for the students.</p>
+               <p>A center for innovation for our students for expermenting on their idea.</p>
+               <p>Laboratory assistances works during regular time.</p>
+               <p>There are also safety measures for protection and preventing damages.</p>
+            </div>
+            <div className='student-img'>
+              <img src='../img/Slide/techLab.gif' alt='' />
+            </div>           
         </div>
     </div>
   )
